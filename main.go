92511d7213e979170ae6e80fe4f77945ed7d90b3
Juana Villalpando <juanavillalpando@juanas-imac.lan>
2020-09-10 23:22:43 -0700

    structures

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import(
-	"fmt"
-)
-func main(){
-	fmt.Println("Hello Word")
+func main() {
+	// printHello()
+	printHuman("Faby", 26, 5, 1)
+	printHuman("Arjun", 26, 5, 1)
+	printHuman("X", 26, 5, 1)
+	printHuman("Y", 26, 5, 1)
+
 }
